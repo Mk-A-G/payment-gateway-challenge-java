9ebca294bab7e10a26d779ee6bdefbe8a0c4d898
Mark Gyimah <mark.gyimah@fdmgroup.com>
2026-04-19 13:55:41 +0100

    add implement validation at controller level and request.

--- src/main/java/com/checkout/payment/gateway/controller/PaymentGatewayController.java
+++ src/main/java/com/checkout/payment/gateway/controller/PaymentGatewayController.java
@@ -1,12 +1,18 @@
 package com.checkout.payment.gateway.controller;
 
+import com.checkout.payment.gateway.enums.PaymentStatus;
+import com.checkout.payment.gateway.model.PostPaymentRequest;
 import com.checkout.payment.gateway.model.PostPaymentResponse;
 import com.checkout.payment.gateway.service.PaymentGatewayService;
 import java.util.UUID;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api")
@@ -19,7 +25,23 @@ public class PaymentGatewayController {
   }
 
   @GetMapping("/payment/{id}")
-  public ResponseEntity<PostPaymentResponse> getPostPaymentEventById(@PathVariable UUID id) {
+  public ResponseEntity<PostPaymentResponse> getPostPaymentEventById(@PathVariable @NotBlank UUID id) {
     return new ResponseEntity<>(paymentGatewayService.getPaymentById(id), HttpStatus.OK);
+  }
+
+  @PostMapping("/payment")
+  public ResponseEntity<PostPaymentResponse> postPaymentEvent(@Valid @RequestBody PostPaymentRequest paymentRequest) {
+
+
+
+    return new ResponseEntity<>(PostPaymentResponse.builder()
+        .id(paymentGatewayService.processPayment(paymentRequest))
+        .amount(paymentRequest.getAmount())
+        .expiryYear(paymentRequest.getExpiryYear())
+        .expiryMonth(paymentRequest.getExpiryMonth())
+        .currency(paymentRequest.getCurrency())
+        .cardNumberLastFour(paymentRequest.getCardNumberLastFour())
+        .status(PaymentStatus.AUTHORIZED)
+        .build(), HttpStatus.OK);
   }
 }
