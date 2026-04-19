9a4168dd1ef4aeef7b855c07dd6616fd951ab970
Mark Gyimah <mark.gyimah@fdmgroup.com>
2026-04-19 13:48:07 +0100

    add tests for new path and request validation

--- src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
+++ src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
@@ -2,66 +2,22 @@ package com.checkout.payment.gateway.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PostPaymentRequest implements Serializable {
 
   @JsonProperty("card_number_last_four")
-  private int cardNumberLastFour;
+  private String cardNumberLastFour;
   @JsonProperty("expiry_month")
   private int expiryMonth;
   @JsonProperty("expiry_year")
   private int expiryYear;
   private String currency;
   private int amount;
-  private int cvv;
-
-  public int getCardNumberLastFour() {
-    return cardNumberLastFour;
-  }
-
-  public void setCardNumberLastFour(int cardNumberLastFour) {
-    this.cardNumberLastFour = cardNumberLastFour;
-  }
-
-  public int getExpiryMonth() {
-    return expiryMonth;
-  }
-
-  public void setExpiryMonth(int expiryMonth) {
-    this.expiryMonth = expiryMonth;
-  }
-
-  public int getExpiryYear() {
-    return expiryYear;
-  }
-
-  public void setExpiryYear(int expiryYear) {
-    this.expiryYear = expiryYear;
-  }
-
-  public String getCurrency() {
-    return currency;
-  }
-
-  public void setCurrency(String currency) {
-    this.currency = currency;
-  }
-
-  public int getAmount() {
-    return amount;
-  }
-
-  public void setAmount(int amount) {
-    this.amount = amount;
-  }
-
-  public int getCvv() {
-    return cvv;
-  }
-
-  public void setCvv(int cvv) {
-    this.cvv = cvv;
-  }
+  private String cvv;
 
   @JsonProperty("expiry_date")
   public String getExpiryDate() {
--- src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
+++ src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
@@ -5,15 +5,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.checkout.payment.gateway.enums.PaymentStatus;
+import com.checkout.payment.gateway.model.PostPaymentRequest;
 import com.checkout.payment.gateway.model.PostPaymentResponse;
 import com.checkout.payment.gateway.repository.PaymentsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
@@ -21,31 +26,142 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 class PaymentGatewayControllerTest {
 
   @Autowired
+  PaymentsRepository paymentsRepository;
+  PostPaymentRequest paymentRequest;
+  @Autowired
   private MockMvc mvc;
   @Autowired
-  PaymentsRepository paymentsRepository;
+  private ObjectMapper objectMapper;
+
+  @BeforeEach
+  void setUp() {
+    paymentRequest = new PostPaymentRequest();
+    paymentRequest.setAmount(10);
+    paymentRequest.setCurrency("USD");
+    paymentRequest.setExpiryMonth(12);
+    paymentRequest.setExpiryYear(2026);
+    paymentRequest.setCardNumberLastFour("4321");
+    paymentRequest.setCvv("123");
+
+  }
 
   @Test
   void whenPaymentWithIdExistThenCorrectPaymentIsReturned() throws Exception {
-    PostPaymentResponse payment = new PostPaymentResponse();
-    payment.setId(UUID.randomUUID());
-    payment.setAmount(10);
-    payment.setCurrency("USD");
-    payment.setStatus(PaymentStatus.AUTHORIZED);
-    payment.setExpiryMonth(12);
-    payment.setExpiryYear(2024);
-    payment.setCardNumberLastFour(4321);
+    PostPaymentResponse paymentResponse = PostPaymentResponse.builder()
+        .id(UUID.randomUUID())
+        .amount(10)
+        .currency("USD")
+        .status(PaymentStatus.AUTHORIZED)
+        .expiryMonth(12)
+        .expiryYear(2026)
+        .cardNumberLastFour("4321").build();
 
-    paymentsRepository.add(payment);
+    paymentsRepository.add(paymentResponse);
 
-    mvc.perform(MockMvcRequestBuilders.get("/payment/" + payment.getId()))
+    mvc.perform(MockMvcRequestBuilders.get("/payment/" + paymentResponse.getId()))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.status").value(payment.getStatus().getName()))
-        .andExpect(jsonPath("$.cardNumberLastFour").value(payment.getCardNumberLastFour()))
-        .andExpect(jsonPath("$.expiryMonth").value(payment.getExpiryMonth()))
-        .andExpect(jsonPath("$.expiryYear").value(payment.getExpiryYear()))
-        .andExpect(jsonPath("$.currency").value(payment.getCurrency()))
-        .andExpect(jsonPath("$.amount").value(payment.getAmount()));
+        .andExpect(jsonPath("$.status").value(paymentResponse.getStatus().getName()))
+        .andExpect(jsonPath("$.cardNumberLastFour").value(paymentResponse.getCardNumberLastFour()))
+        .andExpect(jsonPath("$.expiryMonth").value(paymentResponse.getExpiryMonth()))
+        .andExpect(jsonPath("$.expiryYear").value(paymentResponse.getExpiryYear()))
+        .andExpect(jsonPath("$.currency").value(paymentResponse.getCurrency()))
+        .andExpect(jsonPath("$.amount").value(paymentResponse.getAmount()));
+  }
+
+  @Test
+  void postPaymentEvent() throws Exception {
+
+    paymentRequest.setAmount(10);
+    paymentRequest.setCurrency("USD");
+    paymentRequest.setExpiryMonth(12);
+    paymentRequest.setExpiryYear(2026);
+    paymentRequest.setCardNumberLastFour("4321");
+    paymentRequest.setCvv("123");
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.status").value(PaymentStatus.AUTHORIZED.getName()))
+        .andExpect(jsonPath("$.cardNumberLastFour").value(paymentRequest.getCardNumberLastFour()))
+        .andExpect(jsonPath("$.expiryMonth").value(paymentRequest.getExpiryMonth()))
+        .andExpect(jsonPath("$.expiryYear").value(paymentRequest.getExpiryYear()))
+        .andExpect(jsonPath("$.currency").value(paymentRequest.getCurrency()))
+        .andExpect(jsonPath("$.amount").value(paymentRequest.getAmount()));
+  }
+
+  @Test
+  void postPaymentEventThrowsConstraintViolationErrorsWhenFieldsAreNull() throws Exception {
+
+    paymentRequest = new PostPaymentRequest();
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"", "GHS"})
+  void postPaymentBlankCurrencyReturns400(String currency) throws Exception {
+
+    paymentRequest.setCurrency(currency);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"", "GHS", "2134523457"})
+  void postPaymentInvalidCardNumberReturns400(String lastFour) throws Exception {
+
+    paymentRequest.setCardNumberLastFour(lastFour);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void postPaymentZeroAmountReturns400() throws Exception {
+
+    paymentRequest.setAmount(0);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 24})
+  void postPaymentWrongMonthFormatReturns400(int month) throws Exception {
+
+    paymentRequest.setExpiryMonth(month);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 24, 3002})
+  void postPaymentWrongYearFormatReturns400(int year) throws Exception {
+
+    paymentRequest.setExpiryYear(year);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"1", "24", "3002"})
+  void postPaymentWrongCVVFormatReturns400(String cVV) throws Exception {
+
+    paymentRequest.setCvv(cVV);
+
+    mvc.perform(MockMvcRequestBuilders.post("/payment").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(paymentRequest)))
+        .andExpect(status().isBadRequest());
   }
 
   @Test
