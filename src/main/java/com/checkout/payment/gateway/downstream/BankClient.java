f851aaf25eff9bd635810e0230cb7d70121e93e7
Mark Gyimah <mark.gyimah@fdmgroup.com>
2026-04-19 17:23:56 +0100

    add bank client tests and client with erro handling and full call to donwnstream

--- src/main/java/com/checkout/payment/gateway/downstream/BankClient.java
+++ src/main/java/com/checkout/payment/gateway/downstream/BankClient.java
@@ -1,14 +1,20 @@
 package com.checkout.payment.gateway.downstream;
 
+import com.checkout.payment.gateway.exception.EventProcessingException;
 import com.checkout.payment.gateway.model.BankPaymentRequest;
 import com.checkout.payment.gateway.model.BankPaymentResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BankClient {
 
+  private static final Logger LOG = LoggerFactory.getLogger(BankClient.class);
   @Autowired
   private final RestTemplate restTemplate;
 
@@ -17,7 +23,19 @@ public class BankClient {
   }
 
   public BankPaymentResponse processPayment(BankPaymentRequest request) {
-    return restTemplate.postForObject("",null, BankPaymentResponse.class);
+    LOG.info("Requesting auth from bank client");
+    try {
+      return restTemplate.postForObject("http://localhost:8080/payments", request,
+          BankPaymentResponse.class);
+    } catch (ResourceAccessException e) {
+
+      LOG.error("Bank is unreachable: {}", e.getMessage());
+      throw new EventProcessingException("Error processing payment");
+
+    } catch (HttpServerErrorException e) {
+      LOG.error("Bank returned error status: {}", e.getStatusCode());
+      throw new EventProcessingException("Error processing payment");
+    }
   }
 
 }
