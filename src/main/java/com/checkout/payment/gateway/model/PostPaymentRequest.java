17d19691215b2d6016bba71a99cab390334e4bd2
Mark Gyimah <mark.gyimah@fdmgroup.com>
2026-04-19 14:01:37 +0100

    refactored request to have field validations

--- src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
+++ src/main/java/com/checkout/payment/gateway/model/PostPaymentRequest.java
@@ -1,6 +1,11 @@
 package com.checkout.payment.gateway.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +14,32 @@ import lombok.Setter;
 @Setter
 public class PostPaymentRequest implements Serializable {
 
+  @NotNull(message = "There is a need for the PAN last 4")
+  @Pattern(regexp = "\\d{4}", message = "Must be a 4 digit number")
   @JsonProperty("card_number_last_four")
   private String cardNumberLastFour;
+
+  @Min(1)
+  @Max(12)
   @JsonProperty("expiry_month")
   private int expiryMonth;
+
+  @Min(2026)
+  @Max(3001)
   @JsonProperty("expiry_year")
   private int expiryYear;
+
+  @NotNull(message = "Currency must be USD, GBP or EUR")
+  @Pattern(regexp = "USD|GBP|EUR", message = "Currency must be USD, GBP or EUR")
   private String currency;
+
+  @NotNull
+  @Min(1)
   private int amount;
+
+  @NotNull(message = "There is a need for the CVV ")
+  @NotBlank(message = "There is a need for the CVV ")
+  @Pattern(regexp = "\\d{3}", message = "Must be a 3 digit number")
   private String cvv;
 
   @JsonProperty("expiry_date")
@@ -26,13 +49,9 @@ public class PostPaymentRequest implements Serializable {
 
   @Override
   public String toString() {
-    return "PostPaymentRequest{" +
-        "cardNumberLastFour=" + cardNumberLastFour +
-        ", expiryMonth=" + expiryMonth +
-        ", expiryYear=" + expiryYear +
-        ", currency='" + currency + '\'' +
-        ", amount=" + amount +
-        ", cvv=" + cvv +
-        '}';
+    return "PostPaymentRequest{" + "cardNumberLastFour=" + cardNumberLastFour + ", expiryMonth="
+        + expiryMonth + ", expiryYear=" + expiryYear + ", currency='" + currency + '\''
+        + ", amount=" + amount + ", cvv=" + cvv + '}';
   }
 }
+
